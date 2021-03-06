4a174a1a58464b2ea108238b963ab36545b7b069
Ken McN <mcneillkenneth@gmail.com>
2021-03-06 09:38:57 -0800

    test change

--- src/Control.java
+++ src/Control.java
@@ -14,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
+/** TODO:javadoc */
 public class Control {
 
     private static final float ROUNDF = 100f;
